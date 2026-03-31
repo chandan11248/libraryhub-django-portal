99a585723e89520bbaeb8e0a10a693dc090a080e
chandan shah <letschandansah@gmail.com>
2026-03-31 19:21:51 +0545

    chore(cleanup): remove logs and unused boilerplate

--- library/views.py
+++ library/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
